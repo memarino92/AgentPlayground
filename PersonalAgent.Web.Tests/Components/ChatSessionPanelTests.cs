35a50c14d9af746cdb62207627e17dec3eaef190
Your Name <you@example.com>
2026-04-04 01:26:02 -0400

    feat: refresh chat UI layout and rendering

---
+++ PersonalAgent.Web.Tests/Components/ChatSessionPanelTests.cs
@@ -0,0 +1,87 @@
+using Bunit;
+using FluentAssertions;
+using Microsoft.AspNetCore.Components;
+using PersonalAgent.Web.Components.Pages;
+using PersonalAgent.Web.Services;
+using Xunit;
+
+namespace PersonalAgent.Web.Tests.Components;
+
+public class ChatSessionPanelTests : TestContext
+{
+    [Fact]
+    public void SessionPanel_ShowsEmptyState_WhenNoSessionsExist()
+    {
+        var cut = RenderComponent<ChatSessionPanel>(parameters => parameters
+            .Add(component => component.Sessions, new List<SessionListItem>())
+            .Add(component => component.IsLoadingSessions, false)
+            .Add(component => component.IsBusy, false)
+            .Add(component => component.HasMoreSessions, false)
+            .Add(component => component.SelectedSessionOption, string.Empty)
+            .Add(component => component.FormatSessionOption, new Func<SessionListItem, string>(session => session.Snippet))
+            .Add(component => component.OnSessionSelectionChanged, EventCallback.Factory.Create<string?>(this, _ => Task.CompletedTask))
+            .Add(component => component.OnLoadMoreSessions, EventCallback.Factory.Create(this, () => Task.CompletedTask))
+            .Add(component => component.OnCloseDrawer, EventCallback.Factory.Create(this, () => Task.CompletedTask)));
+
+        cut.Markup.Should().Contain("No chats yet.");
+    }
+
+    [Fact]
+    public void SessionPanel_RendersSessionList_WhenSessionsExist()
+    {
+        var cut = RenderComponent<ChatSessionPanel>(parameters => parameters
+            .Add(component => component.Sessions, new List<SessionListItem> { new("session-1", "First chat", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow) })
+            .Add(component => component.IsLoadingSessions, false)
+            .Add(component => component.IsBusy, false)
+            .Add(component => component.HasMoreSessions, false)
+            .Add(component => component.SelectedSessionOption, string.Empty)
+            .Add(component => component.FormatSessionOption, new Func<SessionListItem, string>(session => session.Snippet))
+            .Add(component => component.OnSessionSelectionChanged, EventCallback.Factory.Create<string?>(this, _ => Task.CompletedTask))
+            .Add(component => component.OnLoadMoreSessions, EventCallback.Factory.Create(this, () => Task.CompletedTask))
+            .Add(component => component.OnCloseDrawer, EventCallback.Factory.Create(this, () => Task.CompletedTask)));
+
+        cut.Find("ul.session-panel__list").Should().NotBeNull();
+        cut.Find("button.session-panel__button").TextContent.Should().Contain("First chat");
+    }
+
+    [Fact]
+    public void SessionPanel_RendersSessionsAndSelectionCallback()
+    {
+        string? selectedSessionId = null;
+
+        var cut = RenderComponent<ChatSessionPanel>(parameters => parameters
+            .Add(component => component.Sessions, new List<SessionListItem> { new("session-1", "First chat", DateTimeOffset.UtcNow, DateTimeOffset.UtcNow) })
+            .Add(component => component.IsLoadingSessions, false)
+            .Add(component => component.IsBusy, false)
+            .Add(component => component.HasMoreSessions, true)
+            .Add(component => component.SelectedSessionOption, string.Empty)
+            .Add(component => component.FormatSessionOption, new Func<SessionListItem, string>(session => session.Snippet))
+            .Add(component => component.OnSessionSelectionChanged, EventCallback.Factory.Create<string?>(this, value => selectedSessionId = value))
+            .Add(component => component.OnLoadMoreSessions, EventCallback.Factory.Create(this, () => Task.CompletedTask))
+            .Add(component => component.OnCloseDrawer, EventCallback.Factory.Create(this, () => Task.CompletedTask)));
+
+        cut.Find("button.session-panel__button").Click();
+        selectedSessionId.Should().Be("session-1");
+        cut.Markup.Should().Contain("Load more");
+    }
+
+    [Fact]
+    public void SessionPanel_InvokesCloseDrawer_WhenCloseClicked()
+    {
+        var closed = false;
+
+        var cut = RenderComponent<ChatSessionPanel>(parameters => parameters
+            .Add(component => component.Sessions, new List<SessionListItem>())
+            .Add(component => component.IsLoadingSessions, false)
+            .Add(component => component.IsBusy, false)
+            .Add(component => component.HasMoreSessions, false)
+            .Add(component => component.SelectedSessionOption, string.Empty)
+            .Add(component => component.FormatSessionOption, new Func<SessionListItem, string>(session => session.Snippet))
+            .Add(component => component.OnSessionSelectionChanged, EventCallback.Factory.Create<string?>(this, _ => Task.CompletedTask))
+            .Add(component => component.OnLoadMoreSessions, EventCallback.Factory.Create(this, () => Task.CompletedTask))
+            .Add(component => component.OnCloseDrawer, EventCallback.Factory.Create(this, () => closed = true)));
+
+        cut.Find("button.session-panel__close").Click();
+        closed.Should().BeTrue();
+    }
+}
--- PersonalAgent.Web/Program.cs
+++ PersonalAgent.Web/Program.cs
@@ -1,10 +1,15 @@
 using AgentPlayground.Contracts.Messaging;
 using AgentPlayground.Contracts.Hosting;
+using Microsoft.AspNetCore.DataProtection;
 using PersonalAgent.Web.Extensions;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.ConfigurePlatformHosting();
 if (builder.Environment.IsDevelopment()) builder.Configuration.AddUserSecrets<Program>();
+var keyRingPath = Path.Combine(builder.Environment.ContentRootPath, "keys");
+Directory.CreateDirectory(keyRingPath);
+builder.Services.AddDataProtection().PersistKeysToFileSystem(new DirectoryInfo(keyRingPath)).SetApplicationName("PersonalAgent.Web");
 builder.Services.AddPersonalAgentApiClient(builder.Configuration);
 builder.Services.AddGitHubAuthentication(builder.Configuration);
 builder.Services.AddPersonalAgentWebMessaging(builder.Configuration);
